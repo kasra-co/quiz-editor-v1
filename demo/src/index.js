7745f0ad36daf5ae37c9e5880810f89fa14cfee2
William Akehurst <william@kasra.co>
2015-04-16 10:10:36 +0300

    Added default quiz data as prop into parent component for demo/src.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,7 +1,13 @@
 var React = require( 'react' );
+var initialQuiz = require('./config/quiz.json');
 
 // Require index.js from the root of the project. That is where our module's interface is specified.
 var Quiz = require('../..').Quiz;
 
 
-React.render( <Quiz updateQuiz={ function( quiz ) { console.log( quiz ); }}/>, document.body );
+React.render(
+	<Quiz
+		initialQuiz={ initialQuiz }
+		updateQuiz={ function( quiz ) {
+			console.log( quiz );
+		}} />, document.body );

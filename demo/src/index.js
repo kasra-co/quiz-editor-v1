65b5780e7afcd29a5c01d9b5e63b2034f61e25c8
Dan Ross <ross9885@gmail.com>
2015-04-19 05:54:15 +0300

    Integrate Joi validation by promoting Quiz state to demo app wrapper
    The app wrapper was necessary because validating state on change while adding error info to state leads to infinite loops.
    
    The quiz validator packaged with kasra-validators is currently unsuitable for use here, because it is designed to validate an article containing a quiz, not a quiz. The validator should be split up when time permits.

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,69 @@
-var React = require( 'react' );
-var initialQuiz = require('./config/quiz.json');
+var React = require( "react" );
+var initialQuiz = require("./config/quiz.json");
+var validators = require( "kasra-validators" );
+var Joi = require( "joi" );
 
-// Require index.js from the root of the project. That is where our module's interface is specified.
-var Quiz = require('../..').Quiz;
+var labels = require("../../labels");
+var Quiz = require("../..").Quiz;
 
+var mediaSchema = Joi.object().keys({
+	image: Joi.string().required(),
+	title: Joi.string().required(),
+	caption: Joi.string().required(),
+	altText: Joi.string().required()
+}).required();
 
-React.render(
-	<Quiz
-		initialQuiz={ initialQuiz }
-		updateQuiz={ function( quiz ) {
-			console.log( quiz );
-		}} />, document.body );
+var quizSchema ={
+	questions: Joi.array().min(1).max(10).items(Joi.object().keys({
+		prompt: Joi.string().required(),
+		media: mediaSchema,
+		answers: Joi.array().items(Joi.string().required()).required(),
+	}).required()).required(),
+	results: Joi.array().items(Joi.object().keys({
+		title: Joi.string().required(),
+		text: Joi.string().required(),
+		media: mediaSchema
+	}).required()).required(),
+};
+
+var Demo = React.createClass({
+	render: function() {
+
+		var errorMessage;
+		if( this.state.invalid ) {
+			errorMessage = (
+				<div className={ "errorMessage open" }>
+					<a className="close" onClick={ this.closeErrorMessage }><i className="icon-cancel"></i></a>
+					<p>{ labels.errorMessage }</p>
+				</div>
+			);
+		}
+
+		return (
+			<div>
+				{ errorMessage }
+				<Quiz
+					quiz = { this.state.quiz }
+					updateQuiz={ function( quiz ) {
+						var result = Joi.validate( quiz, quizSchema );
+
+						console.log( result.error );
+
+						this.setState({
+							invalid: !!result.error,
+							quiz: quiz
+						});
+					}.bind( this )} />
+			</div>
+		);
+	},
+
+	getInitialState: function() {
+		return {
+			invalid: false,
+			quiz: initialQuiz
+		};
+	}
+});
+
+React.render( <Demo/>, document.body );
